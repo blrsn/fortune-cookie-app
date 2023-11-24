f2d33161b2d9e2deb76cb01ebcbf6b0b434371dc
Berin Larson <berin.larson@gmail.com>
2023-11-24 22:37:55 +0530

    Corrected rubocop errors

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Fortune < ApplicationRecord
-    validates :text, presence: true
+  validates :text, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_18_051303) do
+ActiveRecord::Schema[7.1].define(version: 20_231_118_051_303) do
   create_table "fortunes", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
